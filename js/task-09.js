49a1133875c524569aa5435950e8bb5a0274189a
Igor Smelyanets <ismelyanets@yahoo.com>
2022-05-06 00:20:30 +0300

    resolve task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnEl = document.querySelector('button.change-color');
+const spanEl = document.querySelector('span.color');
+
+btnEl.addEventListener('click', onChangebodyColor);
+
+function onChangebodyColor() {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+}
