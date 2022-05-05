6684d23c746af79b61514ec68e3b482e5c449046
Igor Smelyanets <ismelyanets@yahoo.com>
2022-05-05 23:56:15 +0300

    resolve task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('form.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    if (formElements.email.value === "" || formElements.password.value === "") {
+        alert("Все поля формы должны быть заполнены!!!")
+    } else {
+        const formData = {
+        email: formElements.email.value,
+        password: formElements.password.value,
+    }
+        console.log(formData);
+        formEl.reset();
+    }
+    
+}
